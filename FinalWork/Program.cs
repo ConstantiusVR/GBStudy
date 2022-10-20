cae5150aa8c1edcacc9b8f01593c3506ad87f4ed
Constantine Ratkin <r-const@yandex.ru>
2022-10-20 22:32:58 +0300

    uncomment

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,24 +1,24 @@
 ﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 
-// Console.WriteLine("Введите целое число M:");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите целое число N:");
-// int n = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите целое число M:");
+ int m = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите целое число N:");
+ int n = Convert.ToInt32(Console.ReadLine());
 
-// void SeriesOfNumbers (int m, int n)
-// {
-//    if (m>n || m==n) 
-//    {
-//        Console.WriteLine("Введите числа так, чтобы M было меньше N");
-//    }
-//    while (m<=n)
-//    {
-//        Console.WriteLine (m);
-//        m++;
-//    }
-// }
-// Console.WriteLine();
-// SeriesOfNumbers(m,n); 
+ void SeriesOfNumbers (int m, int n)
+ {
+    if (m>n || m==n) 
+    {
+        Console.WriteLine("Введите числа так, чтобы M было меньше N");
+    }
+    while (m<=n)
+    {
+        Console.WriteLine (m);
+        m++;
+    }
+ }
+ Console.WriteLine();
+ SeriesOfNumbers(m,n); 
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
@@ -39,9 +39,12 @@ int SumOfNumbers (int m, int n)
     while (m<n)
     {
         sum = sum + m;
-        
+        m++;
     }
     return sum;
 }
 Console.WriteLine();
 SumOfNumbers(m, n);
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
