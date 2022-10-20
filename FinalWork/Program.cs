a3d8d231d9aaacddd99a8a50c80371e9b2d6dc3b
Constantine Ratkin <r-const@yandex.ru>
2022-10-20 22:41:51 +0300

    complete

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -48,3 +48,16 @@ SumOfNumbers(m, n);
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
+static uint Akkermann(uint n, uint m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akkermann(n - 1, 1);
+    else
+        return Akkermann(n - 1, Akkermann(n, m - 1));
+}
+ 
+Console.WriteLine(Akkermann(1, 2));
+Console.ReadKey();
